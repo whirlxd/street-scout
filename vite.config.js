4a94b15b4c1f2bcb7a68e1ee1de0f3ceeaaeb6f9
whirlxd <ninja21official@gmail.com>
2025-01-11 15:42:17 +0530

    fix major bug

--- vite.config.js
+++ vite.config.js
@@ -4,12 +4,6 @@ import react from "@vitejs/plugin-react-swc";
 // https://vite.dev/config/
 export default defineConfig({
 	plugins: [react()],
-	optimizeDeps: {
-		include: ["aframe", "ar.js"],
-	},
-	build: {
-		commonjsOptions: {
-			include: [/aframe/, /ar.js/],
-		},
-	},
+	
+	
 });
